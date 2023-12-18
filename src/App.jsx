6eccc118780f953f9bf8d02186bd7d5501654890
Nahuel <salvatierra.nahuel@gmai.com>
2023-12-18 15:40:57 -0300

    Add mains components

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ActiveSectionContextProvider from "./context/activeSectionProvider";
+import Layout from "./components/Layout";
+import Intro from "./components/Intro";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import ThemeSwitch from "./components/ThemeSwitch";
+import ThemeContextProvider from "./context/ThemeContextProvider";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+	return (
+		<BrowserRouter>
+		<ThemeContextProvider>
+			<ActiveSectionContextProvider>
+				<Layout>
+					<NavBar />
+					<Intro/>
+					<Projects/>
+					<Skills/>
+					<About/>
+					<Contact/>
+					<ThemeSwitch/>
+				</Layout>
+			</ActiveSectionContextProvider>
+			</ThemeContextProvider>
+		</BrowserRouter>
+	);
+}
+
+export default App;
