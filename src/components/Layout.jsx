21bb94af91322a32ab921eee10baaae1a3912dc2
Nahuel <salvatierra.nahuel@gmail.com>
2025-05-02 21:49:18 -0300

    Refactor Layout, NavBar, and SectionHeading components for improved styling and structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,8 @@ import { Toaster } from "react-hot-toast";
 
 export default function Layout({ children }) {
 	return (
-		<section className="w-full p-0">
-			<div className="bg-gray-50 text-gray-950 relative pt-28 sm:pt-28 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90">
-				<div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-				{children}
-			</div>
+		<section className="w-full h-full bg-gray-50 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 p-0">
+			<div className=" h-full relative">{children}</div>
 			<Toaster position="top-right" />
 		</section>
 	);
