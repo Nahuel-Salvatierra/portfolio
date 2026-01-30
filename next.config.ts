b04c1b0104c81278d5332b4242cb3ad7fcd5183d
Nahuel <salvatierra.nahuel@gmail.com>
2026-01-29 22:00:05 -0300

    Update Next.js configuration with export output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
